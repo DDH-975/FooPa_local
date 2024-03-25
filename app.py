eeab8ce9b5ecb8f7955e5b1fb82818abf2d1b8e3
choiminseok <cak2545@naver.com>
2024-03-25 21:37:49 +0900

    ChatGPT API 얀동

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
+from flask import request, jsonify
+from flask_cors import CORS
+#from dotenv import load_dotenv
+import openai
+import os
+
+
 
 app = Flask(__name__)
 
@@ -26,14 +33,27 @@ def western_page():
 def japanese_page():
     return render_template('japanese.html')
 
-@app.route('/category/gpt')
-def gpt_page():
-    return render_template('gpt.html')
 
 @app.route('/category/basket')
 def basket_page():
     return render_template('basket.html')
 
+dish = "제육볶음"
+@app.route('/gpt', methods =['GET','POST'])
+def chatGPT():
+    # set api key
+    #openai.api_key = os.environ.get("FLASK_API_KEY")
+    openai.api_key = ""
+    # Call the chat GPT API
+    completion = openai.chat.completions.create(
+        model="gpt-4",
+        messages=[
+        {"role": "system", "content": "You are a competent cook who knows Korean, Chinese, Western, and Japanese"},
+        {"role": "user", "content": f"${dish}의 재료와 요리 방법을 알려줘" }
+        ],
+    )
+    result = completion.choices[0].message.content
+    return render_template('gpt.html', result = result)
 
 if __name__ == '__main__':
     app.run()
